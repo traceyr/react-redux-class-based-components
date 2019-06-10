06babcea3f444551647e7350252bedcd5d29ade6
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-06-10 15:51:20 -0700

    added seasonal display plus css. Moved multiple returns out of render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './seasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errMsg: '' };
@@ -9,22 +11,25 @@ class App extends React.Component {
        (position) => this.setState({ lat: position.coords.latitude }),
        (err) => this.setState({ errMsg: err.message })
      );
-    // console.log('My component was rendered to the screen');
   }
 
-  componentDidUpdate() {
-    console.log('My component updated and render was called again.');
-  }
-  //React componment. MUST USE
-  render() {
-    console.log('yipee');
+  renderContent() {
     if(this.state.errMsg && !this.state.lat) {
       return <div>Error: { this.state.errMsg }</div>
     } else if (!this.state.errMsg && this.state.lat){
-      return <div>Latitude: { this.state.lat }</div>
+      return <SeasonDisplay lat={ this.state.lat }/>
     } else {
-      return <div>Loading!</div>
+      return <div><Spinner message="Please accept location request"/></div>
     }
+  }
+  //React componment. MUST USE
+  //No css for this class. Just removing multiple returns from render. showing how to add one property no matter what is rendered.
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
--- src/seasonDisplay.jsx
+++ src/seasonDisplay.jsx
@@ -1,7 +1,37 @@
+import './seasonDisplay.css'
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    iconName: 'massive red sun outline icon'
+  },
+  winter: {
+    text: 'Brr, it is chilly',
+    iconName: 'massive blue snowflake outline icon'
+  }
+}
+
+function seasonDeterminator(lat, month) {
+  console.log(lat);
+  console.log(month);
+  if(month > 2 && month < 9){
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = seasonDeterminator(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon-right ${iconName}`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
