8acd750872803b21b91c5f225c0b6f262e77a9e5
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-06-10 14:44:32 -0700

    updated constructor to only initialize state and used componentdidupdate for finding location

--- src/index.js
+++ src/index.js
@@ -8,18 +8,22 @@ class App extends React.Component {
 
     //This is the only time we directly change state;
     this.state = { lat: null, errMsg: '' };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-       (position) => {
-         this.setState({ lat: position.coords.latitude });
-       },
-       (err) => {
-         this.setState({ errMsg: err.message });
-       }
+       (position) => this.setState({ lat: position.coords.latitude }),
+       (err) => this.setState({ errMsg: err.message })
      );
+    // console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate() {
+    console.log('My component updated and render was called again.');
   }
   //React componment. MUST USE
   render() {
+    console.log('yipee');
     if(this.state.errMsg && !this.state.lat) {
       return <div>Error: { this.state.errMsg }</div>
     } else if (!this.state.errMsg && this.state.lat){
